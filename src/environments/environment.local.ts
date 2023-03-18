cecab6dc709cecd1ed75f96a8c83d50eb7c43a45
tornikeo <tornikeonoprishvili@gmail.com>
2023-03-18 22:28:44 +0400

    Code works, fuckin' finally

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  apiUrl: 'http://localhost:8080/ui',
+  actionUrl: 'http://localhost:4200/login',
+  apiAuthUrl: 'http://localhost:8080/auth',
+  apiHealthCheckUrl: 'http://localhost:8080/health',
+  presentationUrl: 'https://www.beautiful.ai/player/-NPTG7Q4QvdhjZS7EgrS/FollowFoxAI-Neurohumanities-Lab-Duke-Public',
+  firebaseConfig: {
+    apiKey: "AIzaSyBNRoRqMBVDJoWRtOikqK76KVP6HH4IROc",
+    authDomain: "tornikeo.firebaseapp.com",
+    projectId: "tornikeo",
+    storageBucket: "tornikeo.appspot.com",
+    messagingSenderId: "561057686164",
+    appId: "1:561057686164:web:3dd4d58b30bfab0f85ca81"
+  }
+};
